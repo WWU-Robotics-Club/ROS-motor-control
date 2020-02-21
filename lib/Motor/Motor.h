b202af06dbd11e5d51144ec6039aaf081900d3f8
CalebJ2 <calebinvents@gmail.com>
2020-02-20 17:36:17 -0800

    Fix some merge issues

--- lib/Motor/Motor.h
+++ lib/Motor/Motor.h
@@ -1,11 +1,11 @@
-#ifndef ARDUINO_ROS_MOTOR_CONTROL_LIB_MOTOR_MOTOR_H_
-#define ARDUINO_ROS_MOTOR_CONTROL_LIB_MOTOR_MOTOR_H_
-
 // https://www.pjrc.com/teensy/td_libs_Encoder.html
 #include <Encoder.h>
 // https://playground.arduino.cc/Code/PIDLibrary/
 #include <PID_v1.h>
 
+// PID sample time in ms
+#define SAMPLE_TIME 20
+#define OUTPUT_LIMIT 255
 
 /*
  * Motor driver class
@@ -15,33 +15,28 @@
 class Motor {
  public:
   Motor(uint8_t IN1, uint8_t IN2, uint8_t PWM, uint8_t STBY,
-    uint8_t encoderA, uint8_t encoderB,
-    double kp = 2, double ki = 5, double kd = 1);
+    uint8_t encoderA, uint8_t encoderB, bool reverse=0,
+    double kp=200, double ki=50, double kd=0);
+  void init();
   void setPidEnabled(bool enable);
   void setTunings(double kp, double ki, double kd);
-  int32_t getPosition();
+  long getPosition();
   void setVelocity(double vel);  // todo: define units
   void update();  // update the pids and outpute
-
- private:
   void write(double power);  // todo: pick better name?
+ private:
   // Motor driver pin numbers
-  const uint8_t IN1;
-  const uint8_t IN2;
-  const uint8_t PWM;
-  const uint8_t STBY;
+  const uint8_t IN1, IN2, PWM, STBY;
   // Encoder pin numbers
   Encoder* encoder;
-  const uint8_t A;
-  const uint8_t B;
+  const uint8_t A, B;
   int32_t lastPos = 0;
+  int32_t lastTime = 0;
   // PID
   PID* pid;
-  bool pidEnabled = true;
-  double setpoint;  // target velocity // todo: define units
-  double input;  // current velocity // todo: define units
-  // todo: pick what this is. 0-1 or 0-255 or what
-  double output;  // output level
+  bool reverse;
+  double setpoint = 0;  // target velocity // todo: define units
+  double input = 0;  // current velocity // todo: define units
+  double output;  // output level // todo: pick what this is. 0-1 or 0-255 or what
 };
 
-#endif  // ARDUINO_ROS_MOTOR_CONTROL_LIB_MOTOR_MOTOR_H_
